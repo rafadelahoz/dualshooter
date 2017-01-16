3914049518a9f904aa340548503f479df5018613
Rafa de la Hoz <rafa.delahoz@gmail.com>
2017-01-16 13:50:30 +0100

    The player can now rotate around another entity (weirdly, for now) Change executable filename and title and company and thats not useful actually.

--- source/Entities/Player.hx
+++ source/Entities/Player.hx
@@ -1,16 +1,53 @@
 package;
 
+import flixel.FlxG;
+import flixel.math.FlxMath;
+import flixel.math.FlxAngle;
+import flixel.math.FlxPoint;
+
 class Player extends Entity
 {
-    public function new(X : Float, Y : Float, World : World)
+    var rotationTarget : Entity;
+    var rotationCenter : FlxPoint;
+
+    var distance : Float;
+    var radAngle : Float;
+    var radDelta : Float;
+
+    public function new(X : Float, Y : Float, World : World, RotationTarget : Entity)
     {
         super(X, Y, World);
 
         makeGraphic(8, 8, 0xFF00FF4D);
+        centerOffsets(true);
+
+        rotationTarget = RotationTarget;
+        distance = 16;
+
+        radDelta = 0.1;
+        radAngle = 0;
     }
 
     override public function update(elapsed : Float)
     {
+        updateRotationCenter();
+
+        x = rotationCenter.x + FlxMath.fastCos(radAngle) * distance;
+        y = rotationCenter.y + FlxMath.fastSin(radAngle) * distance;
+
+        if (FlxG.keys.pressed.LEFT)
+            radAngle -= radDelta;
+        else if (FlxG.keys.pressed.RIGHT)
+            radAngle += radDelta;
+
         super.update(elapsed);
+    }
+
+    function updateRotationCenter() : Void
+    {
+        if (rotationTarget != null)
+        {
+            rotationCenter = rotationTarget.getMidpoint();
+        }
     }
 }
--- source/States/World.hx
+++ source/States/World.hx
@@ -8,7 +8,10 @@ class World extends FlxTransitionableState
     {
         bgColor = 0xFF0F0F0F;
 
-        add(new Player(100, 100, this));
+        var target : Entity = new Entity(116, 116, this);
+        target.makeGraphic(1, 1, 0xFFFFFFFF);
+        add(target);
+        add(new Player(100, 100, this, target));
 
         super.create();
     }
