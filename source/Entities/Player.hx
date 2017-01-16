d3f3a28e0fe8ede07d270f4fb0321d5fc31eabc6
Rafa de la Hoz <rafa.delahoz@gmail.com>
2017-01-16 16:34:12 +0100

    There are now two players rotating around each other
    Player will now only rotate around other player instance, and their
    angles will be coupled.

--- source/Entities/Player.hx
+++ source/Entities/Player.hx
@@ -1,46 +1,98 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.math.FlxMath;
 import flixel.math.FlxAngle;
 import flixel.math.FlxPoint;
+import flixel.util.FlxSpriteUtil;
 
 class Player extends Entity
 {
-    var rotationTarget : Entity;
+    var id : Int;
+
+    var rotationTarget : Player;
     var rotationCenter : FlxPoint;
 
     var distance : Float;
     var radAngle : Float;
     var radDelta : Float;
 
-    public function new(X : Float, Y : Float, World : World, RotationTarget : Entity)
+    var canvas : FlxSprite;
+    var lineStyle : Dynamic;
+
+    public function new(X : Float, Y : Float, World : World, Id : Int)
     {
         super(X, Y, World);
 
-        makeGraphic(8, 8, 0xFF00FF4D);
-        centerOffsets(true);
+        id = Id;
 
-        rotationTarget = RotationTarget;
-        distance = 16;
+        makeGraphic(8, 8, (id == 0 ? 0xFF00FF4D : 0xFFFF004D));
+
+        distance = 32;
 
         radDelta = 0.1;
         radAngle = 0;
+
+        canvas = world.canvas;
+        if (id == 0)
+            lineStyle = {thickness: 2, color: 0xFF00FF4D};
+        else
+            lineStyle = {thickness: 1, color: 0xFFFF004D};
+    }
+
+    public function setRotationTarget(target : Player)
+    {
+        rotationTarget = target;
+        updateRotationCenter();
+        // Recompute angle from new target
+        radAngle = FlxAngle.angleBetween(this, rotationTarget);
+
+        computePosition();
     }
 
     override public function update(elapsed : Float)
     {
         updateRotationCenter();
 
-        x = rotationCenter.x + FlxMath.fastCos(radAngle) * distance;
-        y = rotationCenter.y + FlxMath.fastSin(radAngle) * distance;
+        var oldAngle : Float = radAngle;
 
-        if (FlxG.keys.pressed.LEFT)
+        if ((id == 0 && FlxG.keys.pressed.LEFT) || (id == 1 && FlxG.keys.pressed.A))
+        {
             radAngle -= radDelta;
-        else if (FlxG.keys.pressed.RIGHT)
+            if (radAngle < 0)
+                radAngle += Math.PI*2;
+            else if (radAngle > Math.PI*2)
+                radAngle -= Math.PI*2;
+
+            rotationTarget.radAngle = radAngle + Math.PI;
+        }
+        else if ((id == 0 && FlxG.keys.pressed.RIGHT) || (id == 1 && FlxG.keys.pressed.S))
+        {
             radAngle += radDelta;
+            if (radAngle < 0)
+                radAngle += Math.PI*2;
+            else if (radAngle > Math.PI*2)
+                radAngle -= Math.PI*2;
+
+            rotationTarget.radAngle = radAngle + Math.PI;
+        }
+
+        if (radAngle != oldAngle)
+            computePosition();
 
         super.update(elapsed);
+    }
+
+    function computePosition() : Void
+    {
+        /*x = rotationCenter.x + Math.cos(radAngle) * distance - origin.x;
+        y = rotationCenter.y + Math.sin(radAngle) * distance - origin.y;
+        last.x = x;
+        last.y = y;*/
+        var pos : FlxPoint = FlxAngle.getCartesianCoords(distance, radAngle * FlxAngle.TO_DEG);
+        x = rotationCenter.x + pos.x - origin.x;
+        y = rotationCenter.y + pos.y - origin.y;
     }
 
     function updateRotationCenter() : Void
@@ -49,5 +101,16 @@ class Player extends Entity
         {
             rotationCenter = rotationTarget.getMidpoint();
         }
+    }
+
+    override public function draw()
+    {
+        super.draw();
+
+        var midpoint : FlxPoint = getMidpoint();
+        FlxSpriteUtil.drawLine(canvas, midpoint.x, midpoint.y,
+                                midpoint.x + Math.cos(radAngle + Math.PI) * distance,
+                                midpoint.y + Math.sin(radAngle + Math.PI) * distance,
+                                lineStyle);
     }
 }
--- source/States/World.hx
+++ source/States/World.hx
@@ -1,18 +1,36 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSpriteUtil;
 import flixel.addons.transition.FlxTransitionableState;
 
 class World extends FlxTransitionableState
 {
+    public var canvas : FlxSprite;
     override public function create()
     {
         bgColor = 0xFF0F0F0F;
 
-        var target : Entity = new Entity(116, 116, this);
-        target.makeGraphic(1, 1, 0xFFFFFFFF);
-        add(target);
-        add(new Player(100, 100, this, target));
+        canvas = new FlxSprite(0, 0).makeGraphic(Std.int(FlxG.width), Std.int(FlxG.height), 0x00000000);
+        add(canvas);
+
+        var player1 : Player = new Player(100, 100, this, 0);
+        var player2 : Player = new Player(100, 100, this, 1);
+
+        add(player1);
+        add(player2);
+
+        player1.setRotationTarget(player2);
+        player2.setRotationTarget(player1);
 
         super.create();
+    }
+
+    override public function draw()
+    {
+        FlxSpriteUtil.fill(canvas, 0x00000000);
+        super.draw();
+        canvas.draw();
     }
 }
